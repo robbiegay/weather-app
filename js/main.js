1b0616312c5e27054037499fa0564231422845d4
Robbie Gay <robbiecgay@gmail.com>
2019-09-24 12:20:02 -0400

    Got API fetch to correctly return city name, temp, and weather status

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
 // JavaScript
+
+let btn = document.querySelector('.btn');
+
+// Test Fields
+let city = document.querySelector('#city');
+let temp = document.querySelector('#temp');
+let status = document.querySelector('#status');
+
+btn.addEventListener('click', getWeather);
+
+async function getWeather() {
+    let zip = document.querySelector('.form-control').value;
+    // alert(`The zip-code is ${zip}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&APPID=15d6e8a17e124db561676b9b0b009aac`);
+    const currentWeather = await response.json();
+    // console.log(JSON.stringify(currentWeather));
+    // console.log(currentWeather);
+    // console.log(typeof currentWeather);
+    // console.log(currentWeather.main.temp);
+    // console.log(currentWeather.weather[0].description);
+    // console.log(currentWeather.name);
+
+    city.innerHTML = currentWeather.name;
+    temp.innerHTML = currentWeather.main.temp;
+    status.innerHTML = currentWeather.weather[0].description;
+
+
+
+    // city name = currentWeather.name
+    // temp = currentWeather.main.temp --> then need to convert from K to F and C
+    // condition = currentWeather.weather.description
+    // console.log(currentWeather.weather.main);
+
+
+}
+
+
+
+
+//API URL: `api.openweathermap.org/data/2.5/weather?zip=${zip},us&APPID=15d6e8a17e124db561676b9b0b009aac`
